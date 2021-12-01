84f96056cd8d9adf28a833696cb0d39d072e6744
Ekaterinabot <slimer246@rambler.ru>
2021-12-01 15:44:33 +0500

    Задание 1 к Видео 14 (разбор Видео 15)

--- Dz1_Video14.py
+++ Dz1_Video14.py
@@ -5,3 +5,18 @@ c = int (5)
 print (a, "+", b, "=", c, sep = "")
 # Выход: 2+2=5.
 # Решение по разбору Видео 15:
+>>> x=1
+>>> dir(x)
+['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes']
+>>> type(x)
+<class 'int'>
+>>> class MyInt(int):
+	def __add__(self,x):
+		return super().__add__(x+1)
+
+	
+>>> y=MyInt(2)
+>>> y+2
+5
+>>> 
+
